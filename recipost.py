c74a402bc5a0ed777849feb8d820ce5847d88943
Peter <peter.degroff@platform46.com>
2010-06-21 17:06:16 +1000

    i dont know

--- recipost.py
+++ recipost.py
@@ -27,7 +27,7 @@ def connect_db():
 
 def init_db():
     with closing(connect_db()) as db:
-        with app.open_resource('schema.sql') as f:
+        with app.open_resource(DATABASE) as f:
             db.cursor().executescript(f.read())
         db.commit()
 
